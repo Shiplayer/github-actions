f1e9598d92dcb960b2a66b6f5aebfc9388af71f0
Shiplayer <shiplayer@yandex.ru>
2020-12-13 16:21:03 +0300

    Create branch_name.py

---
+++ branch_name.py
@@ -0,0 +1,20 @@
+import sys
+from typing import AnyStr
+
+
+def getBranchName(name: AnyStr):
+    if name.__contains__("refs/heads/"):
+        name = name.replace("refs/heads/", "")
+    remove_illegal_file_chars_in_name = ['~', '`', '\'', '"', '!', '%', ';', '']
+    illegal_file_char_in_names = [':', '&', '*', '(', ')', '<', '>', '/', '[', ']', '|', '{', '}', '\\', '\t', '\n']
+    split_name = name.split('/')
+    name = split_name[len(split_name) - 2] + ':' + split_name[len(split_name) - 1]
+    for illegalChar in remove_illegal_file_chars_in_name:
+        name = name.replace(illegalChar, '')
+    for illegalChar in illegal_file_char_in_names:
+        name = name.replace(illegalChar, '.')
+    return name
+
+
+if __name__ == "__main__":
+    print(getBranchName(sys.argv[1]))
